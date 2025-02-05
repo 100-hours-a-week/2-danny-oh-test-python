51e0c5ff5cb9949f0f04760b624dfab4991a32bd
yeachan <dus001228@naver.com>
2025-02-05 15:14:59 +0900

    env: 시크릿키 숨기기 (별 의미는 없음)

--- danny_community/settings.py
+++ danny_community/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x1z^e=y987g!15j6!n94^r4v5ejeij!##+d^((^n&su!0(6a5k'
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
